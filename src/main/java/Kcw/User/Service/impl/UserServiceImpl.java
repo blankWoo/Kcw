2811b3b5727e304bc4533084e146d049f1cce891
kskim-curonsys <kskim@curonsys.com>
2020-05-29 16:12:16 +0900

    gitTest

---
+++ src/main/java/Kcw/User/Service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package Kcw.User.Service.impl;
+
+import Kcw.User.Service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    public int kcwUserLogin(HashMap<String, Object> hashMap) throws Exception {
+        return 0;
+    }
+}
